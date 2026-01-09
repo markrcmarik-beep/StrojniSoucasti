65d13279eabffdb0ac191441a54271ad15b65f98
Martin <markrcmarik@gmail.com>
2026-01-09 22:00:44 +0100

    martialy

---
+++ src/materialy.jl
@@ -0,0 +1,2 @@
+# ver: 2026-01-08
+# This file is now empty - materialy function moved to database.jl
--- src/materialy/Materialy.jl
+++
@@ -1,2 +0,0 @@
-# ver: 2026-01-08
-# This file is now empty - materialy function moved to database.jl
