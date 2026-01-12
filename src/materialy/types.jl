eb6112315c2f4bb44c95234bc59185a1e2b14aec
Martin <markrcmarik@gmail.com>
2026-01-12 16:50:12 +0100

    doplnění dat materialy

--- src/materialy/types.jl
+++ src/materialy/types.jl
@@ -1,14 +1,18 @@
-# ver: 2026-01-08
+# ver: 2026-01-12
 
 struct Material
     name::String
     standard::String
-    Re::Float64          # nominální mez kluzu [MPa]
-    Rm_min::Float64
-    Rm_max::Float64
-    A::Float64           # tažnost [%]
-    KV::Float64          # rázová práce [J]
-    T_KV::Float64        # teplota zkoušky [°C]
-    weldable::Bool
+    Re::Float64         # nominální mez kluzu [MPa]
+    Rm_min::Float64     # mez pevnosti [MPa]
+    Rm_max::Float64     # mez pevnosti max. [MPa]
+    A::Float64          # tažnost [%]
+    KV::Float64         # rázová práce [J]
+    T_KV::Float64       # teplota zkoušky [°C]
+    weldable::Bool      # svařitelnost
     thickness_max::Float64
+    E::Float64          # Modul pružnosti (Youngův modul) [MPa]
+    G::Float64          #
+    ny::Float64         # Poissonovo číslo
+    rho::Float64        # hustota [kg/m^3]
 end
