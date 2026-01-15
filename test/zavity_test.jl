040f147980082fe93e9a3dea1cedf0f05b7cd335
Martin <markrcmarik@gmail.com>
2026-01-15 20:48:53 +0100

    zavity

--- test/zavity_test.jl
+++ test/zavity_test.jl
@@ -3,12 +3,14 @@ using StrojniSoucasti
 
 Z = StrojniSoucasti.Zavity.Zavity()
 
-z1 = Z("M8")
-z1.name        # "M8x1.25"
-z1.pitch       # 1.25
+#z1 = Z("M8")
+#z1.name        # "M8x1.25"
+#z1.pitch       # 1.25
 
-z2 = Z("M10x1")
-z2.pitch       # 1.0
+#z2 = Z("M10x1")
+#z2.pitch       # 1.0
 
 z3 = Z("TR20x4")
-z3.d           # 20.0
+println(z3.name)
+println(z3.d)            # 20.0
+print(z3.pitch )       # 4
