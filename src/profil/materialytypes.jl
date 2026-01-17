bf19bb4c193963ae16c5ec7fc1a6485be9462d27
Martin <markrcmarik@gmail.com>
2026-01-17 21:30:09 +0100

    profil

---
+++ src/profil/materialytypes.jl
@@ -0,0 +1,11 @@
+# ver: 2026-01-17
+
+struct Material
+    name::String        # označení
+    standard::String    # norma
+    a::Float64          # rozměr [mm]
+    b::Float64          # rozměr [mm]
+    t::Float64          # tloušťka [mm]
+    R::Float64          # poloměr [mm]
+    material::String    # materiál
+end
