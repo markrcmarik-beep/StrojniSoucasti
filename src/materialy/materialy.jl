ff41993a2f597b606192e4204a93284ae4185d48
Martin <markrcmarik@gmail.com>
2026-01-26 19:53:44 +0100

    materialy

--- src/materialy/materialy.jl
+++ src/materialy/materialy.jl
@@ -31,7 +31,7 @@ using TOML
 include("materialytypes.jl")
 
 const MATERIALY_DB_EN10025_2 = TOML.parsefile(joinpath(@__DIR__, "materialydatabaseOcelEN10025_2.toml"))
-
+const MATERIALY_DB_CSN = TOML.parsefile(joinpath(@__DIR__, "materialydatabaseOcelCSN.toml"))
 
 function materialy(name::AbstractString)::MaterialOcel
     name = uppercase(strip(name)) # velká písmena
@@ -39,8 +39,8 @@ function materialy(name::AbstractString)::MaterialOcel
     
     if haskey(MATERIALY_DB_EN10025_2, name) # materiál existuje v databázi
     
-    row = MATERIALY_DB_EN10025_2[name]
-    return MaterialOcel(
+        row = MATERIALY_DB_EN10025_2[name]
+        return MaterialOcel(
         get(row, "name", name)::String, # název materiálu
         get(row, "standard", "")::String, # norma (nepovinné)
         Float64(get(row, "Re", 0)), # meze kluzu
@@ -56,8 +56,27 @@ function materialy(name::AbstractString)::MaterialOcel
         Float64(get(row, "ny", 0)), # Poissonovo číslo
         Float64(get(row, "rho", 0)) # hustota
     )
-
-    else
-        return nothing
+    elseif haskey(MATERIALY_DB_CSN, name) # materiál existuje v databázi CSN
+        
+        row = MATERIALY_DB_CSN[name]
+        return MaterialOcel(
+        get(row, "name", name)::String, # název materiálu
+        get(row, "standard", "")::String, # norma (nepovinné)
+        Float64(get(row, "Re", 0)), # meze kluzu
+        Float64(get(row, "Rm_min", 0)), # meze pevnosti
+        Float64(get(row, "Rm_max", 0)), # meze pevnosti max
+        Float64(get(row, "A", 0)), # prodloužení
+        Float64(get(row, "KV", 0)), # houževnatost KV
+        Float64(get(row, "T_KV", 0)), # teplota KV
+        Bool(get(row, "weldable", false)), # svařitelnost
+        Float64(get(row, "thickness_max", 0)), # max tloušťka
+        Float64(get(row, "E", 0)), # modul pružnosti
+        Float64(get(row, "G", 0)), # modul smyku
+        Float64(get(row, "ny", 0)), # Poissonovo číslo
+        Float64(get(row, "rho", 0)) # hustota
+    )
+    #else
+    #    return nothing
     end
+    return nothing
 end
--- test/materialy_test.jl
+++ test/materialy_test.jl
@@ -11,24 +11,38 @@ using StrojniSoucasti
     @test mat.Re == 235
     @test mat.Rm_min == 360
     @test mat.Rm_max == 510
-    @test mat.A == 26.0
-    @test mat.KV == 27.0
-    @test mat.T_KV == 20.0
+    @test mat.A == 26
+    @test mat.KV == 27
+    @test mat.T_KV == 20
     @test mat.weldable == true
-    @test mat.thickness_max == 200.0
-    @test mat.E == 210.0
-    @test mat.G == 81.0
+    @test mat.thickness_max == 200
+    @test mat.E == 210
+    @test mat.G == 81
     @test mat.ny == 0.3
-    @test mat.rho == 7850.0
+    @test mat.rho == 7850
 
     mat = materialy("S235JR+N")
     
     @test mat.name == "S235JR+N"
     @test mat.standard == "EN 10025-2"
-    @test mat.Re == 235.0
+    @test mat.Re == 235
     @test mat.weldable == true
-    @test mat.thickness_max == 200.0
-    @test mat.E == 210.0
-    @test mat.rho == 7850.0
+    @test mat.thickness_max == 200
+    @test mat.E == 210
+    @test mat.rho == 7850
+
+    mat = materialy("11 373")
+
+    @test mat.name == "11 373"
+    @test mat.standard == "ČSN"
+    @test mat.Re == 250
+    @test mat.weldable == true
+    @test mat.rho == 7850
+
+    #mat = materialy("nonexistent_material")
+    #println(mat)
+    #@test mat === nothing
 
 end
+#mat = materialy("nonexistent_material")
+#    println(mat)
