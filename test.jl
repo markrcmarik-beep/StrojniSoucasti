98cfe68f444640f075cecba8b8bf0eca0690cd4f
Martin <markrcmarik@gmail.com>
2026-02-20 08:20:08 +0100

    Create test.jl to run the test suite

--- test.jl
+++ test.jl
@@ -1,12 +1,3 @@
-# Test Suite
+using Pkg
 
-# Here we can write tests for our Julia code.
-
-using Test
-
-# Example test
-function test_example()
-    @test 1 + 1 == 2
-end
-
-test_example()
+Pkg.test()
