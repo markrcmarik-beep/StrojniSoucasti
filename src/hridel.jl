fee3d9befae3b6a95d3852075601801ace837ae2
Martin <markrcmarik@gmail.com>
2026-03-19 19:11:08 +0100

    hridel vypocet02

--- src/hridel.jl
+++ src/hridel.jl
@@ -6,8 +6,8 @@
 # délka (L) a materiál. Tato funkce je užitečná pro organizaci a 
 # uchovávání informací o hřídeli, které mohou být následně použity 
 # pro další výpočty nebo analýzy.
-# ver: 2026-03-18
-## Funkce: nazev_funkce()
+# ver: 2026-03-19
+## Funkce: hridel()
 ## Autor: Martin
 #
 ## Cesta uvnitř balíčku:
@@ -29,9 +29,15 @@
 ## Použité proměnné vnitřní:
 #
 function hridel(; Mk=nothing, D=nothing, d=nothing, L=nothing, 
-    mat=nothing, tauDk=nothing, G=nothing, Re=nothing,
+    mat=nothing, tauDk=nothing, G=nothing, Re=nothing, k=nothing,
     zatizeni::AbstractString="statický", return_text=true)
-
+    # ---------------------------------------------------------
+    # pomocné
+    # ---------------------------------------------------------
+    k_uziv=k
+    # ---------------------------------------------------------
+    # vstupy – jednotky
+    # ---------------------------------------------------------
     if D === nothing || d === nothing || L === nothing || mat === nothing
         error("All parameters (D, d, L, material) must be provided.")
     end
@@ -66,15 +72,83 @@ function hridel(; Mk=nothing, D=nothing, d=nothing, L=nothing,
 
     if D !== nothing && d === nothing
         profil1 = "TRKR $D"
+        info = "hřídel"
+        profil1info = "Hřídel s vnějším průměrem D=$D"
     elseif D !== nothing && d !== nothing
-        profil1 = "TRKR $Dxd"
+        t = (D - d) / 2
+        profil1 = "TRKR $D x $t"
+        info = "hřídel dutý"
+        profil1info = "Dutý Hřídel s vnějším průměrem D=$D a vnitřním průměrem d=$d"
     else
         error("Nesprávné kombinace D a d. Musí být buď D nebo D a d.")
     end
+    # ---------------------------------------------------------
+    # výpočet
+    # ---------------------------------------------------------
+    VV1 = namahanikrut(Mk = Mk, profil = profil1, L0 = L, mat = mat, 
+    tauDk=tauDk, G=G, Re=Re, zatizeni=zatizeni, k=k_uziv,
+    return_text = false)
+    if tauDk === nothing
+        tauDk = VV1[:tauDk] # dovolené smykové napětí v krutu
+    end
+    if G === nothing
+        G = VV1[:G] # smykový modul
+    end
+    if Re === nothing
+        Re = VV1[:Re] # mez kluzu
+    end
+    # ---------------------------------------------------------
+    # výstup
+    # ---------------------------------------------------------
+    VV = Dict{Symbol,Any}()
+    VV[:info] = info
+    VV[:zatizeni] = zatizeni # způsob zatížení
+    VV[:zatizeni_info] = "Způsob zatížení"
+    VV[:Mk] = Mk # krouticí moment
+    VV[:Mk_info] = "Krouticí moment"
+    VV[:D] = D # vnější průměr
+    VV[:D_info] = "Vnější průměr hřídele"
+    VV[:d] = d # vnitřní průměr
+    VV[:d_info] = "Vnitřní průměr hřídele"
+    VV[:k] = k_uziv # uživatelský požadavek bezpečnosti
+    VV[:k_info] = "Uživatelský požadavek bezpečnosti"
+    VV[:Wk] = VV1[:Wk] # průřezový modul v krutu
+    VV[:Wk_info] = "Průřezový modul v krutu"
+    VV[:Wk_str] = VV1[:Wk_str] # textový popis Wk (např. z profilu)
+    VV[:Ip] = VV1[:Ip] # polární moment setrvačnosti
+    VV[:Ip_info] = "Polární moment setrvačnosti"
+    VV[:Ip_str] = VV1[:Ip_str] # textový popis Ip (např. z profilu)
+    VV[:tauDk] = tauDk
+    VV[:tauDk_info] = "Dovolené napětí v krutu"
+    VV[:tau] = VV1[:tau] # smykové napětí v krutu
+    VV[:tau_str] = VV1[:tau_str] # vzorec pro výpočet tau
+    VV[:tau_info] = "Napětí v krutu"
+    VV[:phi] = VV1[:phi] # úhel zkroucení
+    VV[:phi_str] = VV1[:phi_str] # vzorec pro výpočet phi
+    VV[:phi_info] = "Úhel zkroucení"
+    VV[:bezpecnost] = VV1[:k] # součinitel bezpečnosti
+    VV[:bezpecnost_str] = VV1[:k_str] # vzorec pro výpočet součinitele bezpečnosti
+    VV[:bezpecnost_info] = "Součinitel bezpečnosti"
+    VV[:verdict] =  VV1[:verdict] # textové hodnocení bezpečnosti spoje
+    VV[:verdict_info] = "Bezpečnost spoje"
+    VV[:G] = G # smykový modul
+    VV[:G_info] = "Smykový modul"
+    VV[:Re] = Re # mez kluzu
+    VV[:Re_info] = "Mez kluzu"
+    VV[:mat] = mat # materiál
+    VV[:mat_info] = "Materiál"
+    VV[:L0] = L # délka pro výpočet úhlu zkroucení
+    VV[:L0_info] = "Délka pro výpočet zkroucení"
+    VV[:theta] = VV1[:theta] # poměrné zkroucení
+    VV[:theta_str] = VV1[:theta_str] # vzorec pro výpočet poměrného zkroucení
+    VV[:theta_info] = "Poměrné zkroucení"
+    #VV[:profil] = profil === nothing ? "" : profil
+    VV[:profil_info] = profil1info # další info o profilu
 
-    VV = namahanikrut(Mk = Mk, profil = profil1, L0 = L, mat = mat, 
-    zatizeni=zatizeni, return_text = false)
-    
-    return Dict("D" => D, "d" => d, "L" => L, "material" => mat)
-
+    if return_text
+        Dispstr = StrojniSoucasti.hrideltext(VV)
+        return VV, Dispstr
+    else
+        return VV
+    end
 end
---
+++ src/hrideltext.jl
@@ -0,0 +1,76 @@
+## Funkce Julia v1.12
+###############################################################
+## Popis funkce:
+# Výpočet namáhání v krutu pro strojní součásti. Generování 
+# textového výpisu výpočtu.
+# ver: 2026-03-19
+## Funkce: hrideltext()
+## Autor: Martin
+#
+## Cesta uvnitř balíčku:
+# StrojniSoucasti/src/hrideltext.jl
+#
+## Vzor:
+## vystupni_promenne = hrideltext(vstupni_promenne)
+## Vstupní proměnné:
+# VV::Dict{Symbol,Any} - slovník vstupních a výstupních proměnných
+## Výstupní proměnné:
+# lines - textový výstup s popisem výpočtu
+## Použité balíčky
+# Printf: @sprintf
+## Použité uživatelské funkce:
+# profil_text_lines(),
+## Příklad:
+#
+###############################################################
+## Použité proměnné vnitřní:
+#
+using Printf: @sprintf
+
+function namahanikruttext(VV::Dict{Symbol,Any})
+    lines = String[] # pole pro textový výstup
+    push!(lines, "Výpočet: $(VV[:info])") # název výpočtu z VV[:info]
+    push!(lines, "----------------------------------------------------------------")
+    push!(lines, "materiál: $(VV[:mat] === nothing ? "" : string(VV[:mat]))")
+    #append!(lines, profil_text_lines(VV)) # přidat info o profilu
+    push!(lines, "zatížení: $(VV[:zatizeni])") # přidat info o zatížení do textového výstupu
+    push!(lines, "----------------------------------------------------------------")
+    push!(lines, "zadání:") # přidat info o zadání výpočtu
+    push!(lines, @sprintf("Mk = %g   %s", VV[:Mk], VV[:Mk_info]))
+    push!(lines, @sprintf("D = %g   %s", VV[:D], VV[:D_info]))
+    if VV[:d] !== nothing
+        push!(lines, @sprintf("d = %g   %s", VV[:d], VV[:d_info]))
+    end
+    if VV[:k] !== nothing
+        push!(lines, @sprintf("k = %g   %s", VV[:k], VV[:k_info]))
+    end
+    if VV[:Wk] !== nothing
+        push!(lines, @sprintf("Wk = %s = %g   %s", VV[:Wk_str], VV[:Wk], VV[:Wk_info]))
+    end
+    if VV[:Ip] !== nothing
+        push!(lines, @sprintf("Ip = %s = %g   %s", VV[:Ip_str], VV[:Ip], VV[:Ip_info]))
+    end
+    if VV[:tauDk] !== nothing
+        push!(lines, @sprintf("tauDk = %g   %s", VV[:tauDk], VV[:tauDk_info]))
+    end
+    if VV[:G] !== nothing
+        push!(lines, @sprintf("G = %g   %s", VV[:G], VV[:G_info]))
+    end
+    push!(lines,"-----------------------------------------------------------------")
+    push!(lines, "výpočet:")
+    push!(lines, @sprintf("tau = %s = %g   %s", VV[:tau_str], VV[:tau], VV[:tau_info]))
+    if VV[:phi] !== nothing # úhel zkroucení
+        push!(lines, @sprintf("phi = %s = %g   %s", VV[:phi_str],VV[:phi], VV[:phi_info]))
+        push!(lines, @sprintf("phi = %g   %s", uconvert(u"deg", VV[:phi]), VV[:phi_info]))
+    end
+    if VV[:theta] !== nothing # poměrné zkroucení
+        push!(lines, @sprintf("theta = %s = %g   %s", VV[:theta_str], VV[:theta], 
+            VV[:theta_info]))
+        push!(lines, @sprintf("theta = %g   %s", uconvert(u"deg/m", VV[:theta]), 
+            VV[:theta_info]))
+    end
+    push!(lines, @sprintf("k = %s = %g   %s\n%s:  %s", VV[:bezpecnost_str], 
+        VV[:bezpecnost], VV[:bezpecnost_info], VV[:verdict_info], VV[:verdict]))
+
+    return join(lines, "\n")
+end
